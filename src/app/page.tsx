bd0fe648d9f1f59fd76b9999a6d7a77d19604a12
Luke <lrsorrenti@hotmail.com>
2024-04-25 06:55:33 -0500

    Added multistep loader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import { SignupForm } from "@/components/SignupForm";
+import { MultiStepLoader } from "@/components/MultiStepLoader";
 export default function Home() {
   return (
     <div className="" >
       <div className="flex justify-center mb-8 mt-8">
       {/* <SignupForm/> */}
+      {/* <MultiStepLoader/> */}
       </div>
     </div>
   );
